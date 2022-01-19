13f8fa610620a3ea236d811e5c2d082be45436b1
Michał Zieliński (dev) <michal.zielinski@crafton.pl>
2022-01-19 19:51:33 +0100

    undefined vs. null

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React, { useRef } from 'react';
+import React, { createRef, useRef } from 'react';
 
 import './App.css';
 
 const App = () => {
   const textInputRef = useRef();
+  const numberInputRef = createRef();
 
   const focusOnInput = () => textInputRef.current.focus();
+
+  console.log(textInputRef.current);
+  console.log(numberInputRef.current);
 
   return (
     <div>
       <input
         ref={textInputRef}
         type="text"
+      />
+      <input
+        ref={numberInputRef}
+        type="number"
       />
       <button onClick={focusOnInput}>
         Ustaw focus na input
